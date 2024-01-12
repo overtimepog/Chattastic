c4a83f0194b38de91132d3fb9d50c848d3d6e561
Overtime <Truen2005@gmail.com>
2024-01-12 18:29:19 -0500

    added bat file and python exe

--- run.bat
+++ run.bat
@@ -1,19 +1,20 @@
 @echo off
-echo Checking for Chattastic installation...
+echo Checking for Chattastic...
 
 :: Check for Python installation
 python --version > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python is not installed. Please install Python 3.x first.
-    exit /b
+    echo Python is not installed. installing Python...
+    :: run python installer
+    python-3.11.0-amd64.exe /passive PrependPath=1
 )
 
-:: Check if Chattastic directory exists
-if exist Chattastic (
-    echo Chattastic directory already exists. Skipping cloning.
+:: Check if main.py exists
+if exist main.py (
+    echo main.py found. Chattastic is already cloned.
     goto InstallDependencies
 ) else (
-    echo Cloning Chattastic repository...
+    echo main.py not found. Cloning Chattastic repository...
     git clone https://github.com/overtimepog/Chattastic.git
 )
 
@@ -23,7 +24,7 @@ cd Chattastic
 
 :: Install dependencies
 echo Installing dependencies...
-python -m pip install dearpygui pyaudio gtts playsound@git+https://github.com/taconi/playsound requests sounddevice flask
+python -m pip install dearpygui pyaudio gtts playsound@git+https://github.com/taconi/playsound requests sounddevice flask numpy
 
 :: Run the application
 echo Starting Chattastic...
